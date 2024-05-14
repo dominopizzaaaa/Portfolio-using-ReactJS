556affd7fa88889aef71ab5cbe1303cb8372d955
dominopizzaaaa <94787443+dominopizzaaaa@users.noreply.github.com>
2024-05-14 12:05:10 +0800

    Add files via upload

---
+++ finalised/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Contact from "./Contact"
+import Information from "./Information"
+import Intro from "./Intro"
+import './App.css';
+
+function App() {
+  return (
+    <div className="element">
+      <Intro />
+      <Contact />
+      <Information />
+    </div>
+  )
+}
+
+
+export default App;
